1579e6cd5c66ff682adc8024cf6978a291eac24b
Johnstone Ananda <johnolwamba@gmail.com>
2021-09-10 21:02:51 -0500

    set variables to public

--- Example/MySwiftPackageExample/ContentView.swift
+++ Example/MySwiftPackageExample/ContentView.swift
@@ -6,11 +6,13 @@
 //
 
 import SwiftUI
+import MySwiftPackage
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        WindowGroup {
+            SwiftUIView()
+        }
     }
 }
 
--- Sources/SwiftUIView.swift
+++ Sources/SwiftUIView.swift
@@ -9,15 +9,9 @@ import Foundation
 import SwiftUI
 
 @available(iOS 13.0.0, *)
-struct SwiftUIView: View {
-    var body: some View {
+public struct SwiftUIView: View {
+    public init() {}
+    public var body: some View {
         Text("THIS IS THE SWIFTUIVIEW FROM THE PACKAGE")
-    }
-}
-
-@available(iOS 13.0.0, *)
-struct SwiftUIView_Previews: PreviewProvider {
-    static var previews: some View {
-        SwiftUIView()
     }
 }
